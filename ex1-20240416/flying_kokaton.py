9a89c80895cbd474184531da70d85b7096e10bce
orgil <c0a23006cb@edu.teu.ac.jp>
2024-04-16 18:30:25 +0900

    enshuu 4

--- ex1-20240416/flying_kokaton.py
+++ ex1-20240416/flying_kokaton.py
@@ -30,8 +30,8 @@ def main():
             kk_rct.move_ip([-1, 0])
         if key_lst[pg.K_RIGHT]:
             kk_rct.move_ip([+2, 0]) # enshuu 1
-        else: #enshuu 1
-            kk_rct.move_ip([-1,0]) #enshuu 1
+        else:
+            kk_rct.move_ip([-1,0])
         x = tmr%3200
         screen.blit(bg_img,[-x, 0])
         screen.blit(bg_img2,[-x+1600, 0])
